6851e64310b2158f252f7d65d9e01166ed511046
YogeshSDeshmane <yogeshsdeshmane@gmail.com>
2024-02-10 12:36:11 +0530

    Study

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//Dates
+
+/*
+let myDate = new Date();
+ console.log(myDate);
+
+ console.log(myDate.toString())
+
+ console.log(myDate.toDateString());
+
+ console.log(myDate.toTimeString());
+
+ console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+*/
+
+// let myCreatedDate = new Date(1998,10,16);
+// let myCreatedDate1 = new Date("11-16-2024")
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate1.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate1.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+console.log(newDate.getSeconds());
