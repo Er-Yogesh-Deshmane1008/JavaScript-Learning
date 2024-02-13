d08d4ca35ba85718aae88caf51434012a52f8768
YogeshSDeshmane <yogeshsdeshmane@gmail.com>
2024-02-13 17:02:52 +0530

    Study

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,53 @@
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
+
+/*
+const month = 8
+switch (month) {
+    case 1:
+       console.log("January"); 
+        break;
+    case 2:
+       console.log("Feb"); 
+        break;
+    case 3:
+       console.log("March"); 
+        break;
+    case 4:
+       console.log("April"); 
+        break;
+        
+
+    default:
+        console.log("defaul case match ");
+        break;
+}*/
+
+/*
+const month1 = "feb"
+switch (month1) {
+    case "jan":
+       console.log("January"); 
+        break;
+    case "feb":
+       console.log("Feb"); 
+        break;
+    case "March":
+       console.log("March"); 
+        break;
+    case "April":
+       console.log("April"); 
+        break;
+        
+
+    default:
+        console.log("defaul case match ");
+        break;
+}*/
