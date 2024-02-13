012072f92507e5faf30c3869e9b93692fd7435fd
YogeshSDeshmane <yogeshsdeshmane@gmail.com>
2024-02-13 16:51:09 +0530

    Study

---
+++ 04_control_flow/01_control_flow.js
@@ -0,0 +1,36 @@
+
+//if
+
+// < , >, >=, <=, !=, ==, ===, !==
+/*
+if(true){
+    // console.log(`WELCOME TO JAVASCRIPT`);
+}
+
+if(false){
+    // console.log(`WELCOME TO JAVASCRIPT`);
+}
+// console.log("I am happy");
+
+if(2=="2"){
+    // console.log(`Executed`);
+}
+if(2==="2"){
+    // console.log(`Executed`);
+}
+// console.log("Not Exceuted");
+*/
+
+// if(2!==3){
+//     console.log(`Executed`);
+// }
+
+
+const balance = -1;
+// if(balance >= 500) console.log(`I have ${balance} Rs balance`);
+
+if(balance>=500){
+   console.log(` balances is ${balance}Rs present`);
+}else if(balance<=500){
+    console.log(`balance is ${balance}Rs`);
+}
